8168dcbbf1dbe7a292935ca7f725d4971043d374
neil.hewitt <neil.hewitt@gmail.com>
2016-08-10 17:37:05 +0100

    Initial commit

---
+++ src/Tetris.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Euromoney Institutional Investor Plc")]
+[assembly: AssemblyProduct("Tetris.Core")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bef7afd8-3c85-4826-ae7c-e3495cabbe8e")]
---
+++ src/Tetris.Core/Tetromino.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tetris.Core
+{
+    public enum TetrominoColour
+    {
+        Red, Magenta, Yellow, Cyan, Blue, LightBlue, LightGrey, Lime
+    }
+
+    public class Tetromino
+    {
+        public static readonly Tetromino I = new Tetromino("1111", TetrominoColour.Red);
+        public static readonly Tetromino J = new Tetromino("111\n001", TetrominoColour.Magenta);
+        public static readonly Tetromino L = new Tetromino("111\n100", TetrominoColour.Yellow);
+        public static readonly Tetromino O = new Tetromino("11\n11", TetrominoColour.Cyan);
+        public static readonly Tetromino S = new Tetromino("011\n110", TetrominoColour.Blue);
+        public static readonly Tetromino T = new Tetromino("111\n010", TetrominoColour.LightGrey);
+        public static readonly Tetromino Z = new Tetromino("110\n011", TetrominoColour.Lime);
+
+        public string Pattern { get; private set; }
+        public TetrominoColour Colour { get; private set; }
+
+        internal Tetromino(string shape, TetrominoColour colour)
+        {
+            Pattern = shape;
+            Colour = colour;
+        }
+    }
+}
